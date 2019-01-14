d97dc0484a3bee6549c7b27ede36f7a2b1ae2545
Kristy Currier <kristy.currier@gmail.com>
2019-01-14 14:44:32 -0500

    Added Product controller

---
+++ LabJJ2/Models/ProductsModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LabJJ2.Models
+{
+    public class ProductsModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+
+    }
+}
